b7127e52d9e2f46d0569132f8ed058a3f9737145
NOHA <nohaibrahiim643@gmail.com>
2023-03-26 00:18:06 +0200

    create movies list watch_list

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/home/homeScreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      title: 'Movies App',
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen()
+      },
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
